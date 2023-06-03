d580f035f2c9198efaf2b07d9a4ddc49b3d24c58
Прасолов Вадим <taZik-616@yandex.ru>
2023-06-03 07:18:15 +0500

    Fix empty images

--- api/views/subcategories_bind_random_product.py
+++ api/views/subcategories_bind_random_product.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
-from api.models import Category, CategoryLevel, Product
+from api.models import Category, CategoryLevel, Product, ProductImage
 
 
 @api_view(['GET'])
@@ -11,9 +11,12 @@ def subcategoriesBindRandomProduct(request):
         subCats = Category.objects.filter(level=CategoryLevel.SUBCATEGORY)
         for subCat in subCats:
             # order_by('?') - рандомная сортировка
-            randomProduct = Product.objects.filter(
-                subcategoryId=subCat.categoryId
-            ).order_by('?').first()
+            withImages = ProductImage.objects.values_list('imageId', flat=True)
+            items = Product.objects.filter(
+                subcategoryId=subCat.categoryId,
+                productId__in=withImages
+            ).filter()
+            randomProduct = items.order_by('?').first()
             subCat.subcategoryPreviewProduct = randomProduct
             subCat.save()
         return Response({'success': 'Операция прошла успешно'})
